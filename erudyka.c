72a22ffc6ed2d9363d5532d3276e1aa014d2a2cc
Jlll1 <71319302+Jlll1@users.noreply.github.com>
2022-04-27 20:37:50 +0200

    Add ability to create new cards (#3)
    * Added `-n <content>` command
    * Added argument parsing

--- erudyka.c
+++ erudyka.c
@@ -1,4 +1,46 @@
-int main(int argc, char const *argv[])
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char *erudykaDbPath;
+
+char *
+getErudykaDbPath(void)
 {
+    char *result= getenv("HOME");
+    strcat(result, "/.erudyka/main.edk");
+
+    return result;
+}
+
+int
+addNewCard(const char *content)
+{
+    FILE *db;
+
+    if (strlen(content) > 500 - 2) return -1;
+
+    db = fopen(erudykaDbPath, "a");
+    if (db == NULL) return -1;
+
+    fprintf(db, "%-500s\n", content);
+
+    return 0;
+}
+
+int
+main(int argc, char const *argv[])
+{
+    erudykaDbPath = getErudykaDbPath();
+
+    for (int i = 1; i < argc; i++) {
+        /*
+         * 1 parameter arguments
+         */
+        if (!strcmp(argv[i], "-n")) {   /* Add a new record */
+            return addNewCard(argv[++i]);
+        }
+    }
+
     return 0;
 }
