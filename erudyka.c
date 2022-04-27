ffe0fcc033dd87f8f7a1f2e275f0267f1317fa86
Jlll1 <arghantentua@tutanota.com>
2022-04-27 19:29:17 +0200

    Implement getErudykaPath

--- erudyka.c
+++ erudyka.c
@@ -1,4 +1,19 @@
-int main(int argc, char const *argv[])
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char *
+getErudykaPath(void)
 {
+    char *userHome = getenv("HOME");
+    strcat(userHome, "/.eduryka/");
+
+    return userHome;
+}
+
+int
+main(int argc, char const *argv[])
+{
+    printf("%s\n", getErudykaPath());
     return 0;
 }
