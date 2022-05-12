2d20a7e86273e3001e70adde49f4f47983a494ea
Jlll1 <arghantentua@tutanota.com>
2022-05-12 18:55:01 +0200

    Create erudyka helper function

--- test.sh
+++ test.sh
@@ -41,7 +41,7 @@ test "new <card> -> search <card> returns <card> with id 1"
     expectedCard="1: test card"
 
     erudyka new "$card"
-    result=$(./erudyka --directory test/ search "$card")
+    result=$(erudyka search "$card")
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -50,8 +50,8 @@ test "new <card> -> get 1 returns <card>"
     card="test card"
     expectedCard="test card"
 
-    ./erudyka --directory test/ new "$card"
-    result=$(./erudyka --directory test/ get 1)
+    erudyka new "$card"
+    result=$(erudyka get 1)
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -61,10 +61,10 @@ test "get prints linked cards"
     card2="test card 2"
     expectedCard=$(echo "test card 1\ntest card 2")
 
-    ./erudyka --directory test/ new "$card1"
-    ./erudyka --directory test/ new "$card2"
-    ./erudyka --directory test/ link 2 1
-    result=$(./erudyka --directory test/ get 1)
+    erudyka new "$card1"
+    erudyka new "$card2"
+    erudyka link 2 1
+    result=$(erudyka get 1)
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -74,9 +74,9 @@ test "search doesn't return unrelated results"
     card2="unrelated card"
     expectedCard="1: test card 1"
 
-    ./erudyka --directory test/ new "$card1"
-    ./erudyka --directory test/ new "$card2"
-    result=$(./erudyka --directory test/ search "test")
+    erudyka new "$card1"
+    erudyka new "$card2"
+    result=$(erudyka search "test")
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -86,11 +86,11 @@ test "search displays ids"
     card2="abc"
     expectedCard=$(echo "1: test\n\n3: test\n\n4: test")
 
-    ./erudyka --directory test/ new "$card1"
-    ./erudyka --directory test/ new "$card2"
-    ./erudyka --directory test/ new "$card1"
-    ./erudyka --directory test/ new "$card1"
-    result=$(./erudyka --directory test/ search "test")
+    erudyka new "$card1"
+    erudyka new "$card2"
+    erudyka new "$card1"
+    erudyka new "$card1"
+    result=$(erudyka search "test")
 
     assert_equal "$expectedCard" "$result"
     teardown
