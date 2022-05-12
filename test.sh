51c62e6bd29da77871a63fb48b4f0077565a124f
Jlll1 <arghantentua@tutanota.com>
2022-05-12 22:32:53 +0200

    Expand test suite (#17)
    * added `erudyka` helper function
    * added more tests for `search`, `get` and `save`

--- test.sh
+++ test.sh
@@ -12,6 +12,10 @@ teardown() {
     setup
 }
 
+erudyka() {
+    ./erudyka --directory test/ "$@"
+}
+
 assert_equal() {
     expected="$1"
     actual="$2"
@@ -19,7 +23,7 @@ assert_equal() {
     if [ "$actual" = "$expected" ]; then
         echo "PASS"
     else
-        echo "FAIL - Expected '$expcetd', but got '$actual'."
+        echo "FAIL - Expected '$expected', but got '$actual'."
     fi
 
     echo ""
@@ -36,8 +40,8 @@ test "new <card> -> search <card> returns <card> with id 1"
     card="test card"
     expectedCard="1: test card"
 
-    ./erudyka --directory test/ new "$card"
-    result=$(./erudyka --directory test/ search "$card")
+    erudyka new "$card"
+    result=$(erudyka search "$card")
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -46,8 +50,8 @@ test "new <card> -> get 1 returns <card>"
     card="test card"
     expectedCard="test card"
 
-    ./erudyka --directory test/ new "$card"
-    result=$(./erudyka --directory test/ get 1)
+    erudyka new "$card"
+    result=$(erudyka get 1)
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -55,12 +59,17 @@ test "new <card> -> get 1 returns <card>"
 test "get prints linked cards"
     card1="test card 1"
     card2="test card 2"
-    expectedCard=$(echo "test card 1\ntest card 2")
+    card3="test card 3"
+    card4="test card 4"
+    expectedCard=$(echo "test card 1\ntest card 2\ntest card 4")
 
-    ./erudyka --directory test/ new "$card1"
-    ./erudyka --directory test/ new "$card2"
-    ./erudyka --directory test/ link 2 1
-    result=$(./erudyka --directory test/ get 1)
+    erudyka new "$card1"
+    erudyka new "$card2"
+    erudyka new "$card3"
+    erudyka new "$card4"
+    erudyka link 2 1
+    erudyka link 4 1
+    result=$(erudyka get 1)
 
     assert_equal "$expectedCard" "$result"
     teardown
@@ -70,9 +79,69 @@ test "search doesn't return unrelated results"
     card2="unrelated card"
     expectedCard="1: test card 1"
 
-    ./erudyka --directory test/ new "$card1"
-    ./erudyka --directory test/ new "$card2"
-    result=$(./erudyka --directory test/ search "test")
+    erudyka new "$card1"
+    erudyka new "$card2"
+    result=$(erudyka search "test")
+
+    assert_equal "$expectedCard" "$result"
+    teardown
+
+test "search displays ids"
+    card1="test"
+    card2="abc"
+    expectedCard=$(echo "1: test\n\n3: test\n\n4: test")
+
+    erudyka new "$card1"
+    erudyka new "$card2"
+    erudyka new "$card1"
+    erudyka new "$card1"
+    result=$(erudyka search "test")
+
+    assert_equal "$expectedCard" "$result"
+    teardown
+
+test "search is case insensitive"
+    card1="TEST"
+    expectedCard="1: TEST"
+
+    erudyka new "$card1"
+    result=$(erudyka search "test")
+
+    assert_equal "$expectedCard" "$result"
+    teardown
+
+test "save"
+    echo '#!/bin/sh
+echo "abc $1"' > test/scripts/t.sh
+    card1="test"
+    expectedCard="abc test"
+
+    erudyka save "$card1" t
+    result=$(erudyka get 1)
+
+    assert_equal "$expectedCard" "$result"
+    teardown
+
+test "get trims whitespace"
+    card1="test\ntest  \t \n   \n  \n"
+    expectedCard=$(echo "test\ntest")
+
+    erudyka new "$card1"
+    result=$(erudyka get 1)
+
+    assert_equal "$expectedCard" "$result"
+    teardown
+
+test "search trims whitespace"
+    card1="test\ntest  \t \n   \n  \n"
+    card2="test\ntest2 \n  \n"
+    card3="test"
+    expectedCard=$(echo "1: test\ntest\n\n2: test\ntest2\n\n3: test")
+
+    erudyka new "$card1"
+    erudyka new "$card2"
+    erudyka new "$card3"
+    result=$(erudyka search test)
 
     assert_equal "$expectedCard" "$result"
     teardown
